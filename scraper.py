67f70f483ca680c32ce2e0065c08fd50fb8c7ea1
bahmanh <bahmanhayat@gmail.com>
2016-01-15 02:53:24 -0800

    Shorten the URL

--- scraper.py
+++ scraper.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-r = requests.get("https://sunspot.sdsu.edu/schedule/search?mode=search&period=20162&admin_unit=R&abbrev=CS&number=560&suffix=&courseTitle=&scheduleNumber=&units=&instructor=&facility=&space=&meetingType=&startHours=&startMins=&endHours=&endMins=&remaining_seats_at_least=&ge=")
-
+r = requests.get("https://sunspot.sdsu.edu/schedule/search?mode=search&period=20162&scheduleNumber=21002")
 data = r.text
 soup = BeautifulSoup(data, "html.parser")
 seatData = soup.find_all("div",{"class": "sectionFieldSeats column"})[1].getText('\n').strip()
