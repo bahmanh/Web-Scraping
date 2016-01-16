0f7c96ca4dcf119a16739e25424690aff11f3492
bahmanh <bahmanhayat@gmail.com>
2016-01-16 03:08:55 -0800

    parse out number of available seats

--- scraper.py
+++ scraper.py
@@ -11,9 +11,11 @@ def getData():
     className = soup.find_all("a", {"href": "sectiondetails?scheduleNumber=21002&period=20162&admin_unit=R"})[0].text
     seatData = soup.find_all("div",{"class": "sectionFieldSeats column"})[1].getText('\n').strip()
 
-    print "=========================="+'\n'
-    print className
-    print seatData + '\n'
+   # print "=========================="+'\n'
+   # print className
+   # print seatData + '\n'
+    availableSeats = seatData.split('/')
+    print availableSeats[0]
 
 def freshData():
     while True:
